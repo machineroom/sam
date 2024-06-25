84027ce0e99b656bfb7841a0bbb43969ddb5aea8
James Wilson <jawilso3@synamedia.com>
2024-06-25 20:17:48 +0100

    file reading stuff

--- hello.c
+++ hello.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
 // this is another comment
 
 int global=40;
 
 void f1 (int p1, int p2) {
     global = 50;
+    if (global > 50) {
+        printf ("someting went wrong\n");
+    } else {
+        printf ("all good\n");
+    }
+    // = assigns
+    // == equality?
+    // >=, <=
+    if (global == 1) {
+
+    } else if (global == 2) {
+
+    }
+    switch (global) {
+        case 1:
+        break;
+        case 2:
+        break;
+        case 50:
+        printf ("all good since 50\n");
+        break;
+        case 51:
+        printf ("bad since 51\n");
+        break;
+        default:
+        break;
+    }
+
     printf ("f1 %d\n", p1 + p2);
 }
 
@@ -12,23 +47,57 @@ int f2 (int p1, int p2) {
     return p1 + p2 + global;
 }
 
-void f3 (int p1, int p2, int *p3, int *p4) {
-    *p3 = p1 + p2 + global;
+// p1,p2 are "passed by value"
+// p1,p2 are "passed by reference"
+void f3 (int p1, int p2, int p3, int *p4) {
+    p3 = p1 + p2 + global;
     *p4 = p1 + p2;
+}
+
+unsigned char *read_file() {
+    unsigned char *p;
+    int fd = open ("tmp.txt", O_RDONLY);
+    if (fd < 0) {
+        printf ("Error from open!\n");
+        return NULL;
+    }
+    struct stat fstat_info;
+    int rc = fstat(fd, &fstat_info);
+    if (rc < 0) {
+        printf ("Error from fstat!\n");
+        return NULL;
+    }
+    size_t size = fstat_info.st_size;
+    printf ("file size = %zu\n", size);
+    p = malloc (size);
+    if (p != NULL) {
+        read (fd, p, size);
+    } else {
+        printf ("failed malloc\n");
+        return NULL;
+    }
+    printf ("Read %s from file\n", p);
+    return p;
 }
 
 int main (int argc, char **argv) {
     char i;
     char *j;
 
+    i = 200;
+    printf ("i as signed = %hhd\n",i);
+    printf ("i as unsigned = %hhu\n",i);
+
+    read_file();
+    return 0;
     i = 1;
     j = &i;
 
     f1(7,4);
     int r = f2(3,7);
-    int r1,r2;
+    int r1=0,r2=0;
     printf ("f2 = %d\n", r);
-    f3 (3,7,&r1,&r2);
+    f3 (3,7,(int)&r1,&r2);
     printf ("f3 = %d %d\n", r1,r2);
 
     char ***stupid = &argv;
